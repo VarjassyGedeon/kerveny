85c8b39da6f334595d38f104196f1245416854d3
Varjassy Gedeon <vgedone@gmail.com>
2025-07-10 17:36:34 +0200

    Delete demo directory

--- demo/src/main/java/com/example/demo/Appuser/AppUserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.Appuser;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface AppUserRepository extends JpaRepository<AppUser, Long> {
-    Optional<AppUser> findByEmail(String email);
-}
