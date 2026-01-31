e2ef74699182612040159c47a4cbba5ce2b8a20a
Varjassy Gedeon <vgedone@gmail.com>
2026-01-31 11:22:46 +0100

    Delete src directory

--- src/main/java/com/example/demo/Tantargy/TantargyRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.demo.Tantargy;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TantargyRepository extends JpaRepository<Tantargy, String> {
-}
